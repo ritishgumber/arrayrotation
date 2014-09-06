5d01efd9e21b0cd7bb6c5cdb39c771106c213b8e
Ritish Gumber <cool.ritish@gmail.com>
2014-09-06 23:45:10 +0530

    Update sorce_code.c

--- sorce_code.c
+++ sorce_code.c
@@ -1,20 +1,24 @@
 #include<conio.h>
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
 int y,x,k,l,i,m,r,t;
-int a[10],e[10],d[10];
+int *a,*e,*d;
 char c[10];
 clrscr();
 scanf("%d %d",&t,&m);
+a=(int*)malloc(t*sizeof(int));
+e=(int*)malloc(t*sizeof(int));
+d=(int*)malloc(m*sizeof(int));
 for(i=0;i<t;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d",a+i);
 }
 for(i=0;i<m;i++)
 {
 c[i]=getche();
-scanf("%d",&d[i]);
+scanf("%d",d+i);
 }
 
 for(r=0;r<m;r++)
@@ -36,8 +40,7 @@ case 99 :{
 	{
 	e[i]=a[k];
 	i++;
-	}for(k=0;k<t;k++)
-	{printf("   %d",e[k]);}
+	}
 	for(k=0;k<t;k++)
 	a[k]=e[k];
 	break;}
@@ -51,8 +54,7 @@ case 97 :{for(i=0,y=x;i<x;i++)
 	for(k=0;k<t;k++)
 	{
 	a[k]=e[k];
-	}for(k=0;k<t;k++)
-	printf(" \n %d",a[k]);
+	}
 	break;
 }}   }
 getch();
